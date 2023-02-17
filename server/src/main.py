1f6a70cdd20e31bd5ee147da29dbc4227811b4b0
Vincent C. Mader <vincent.mader@icloud.com>
2023-02-17 14:28:58 +0100

    added css & included a js script

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(
+    __name__,
+    static_url_path='',
+    template_folder="../templates",
+    static_folder="../static",
+)
 
 
 @app.route("/")
---
+++ server/static/js/script.js
@@ -0,0 +1,27 @@
+const sleep = ms => new Promise((r) => setTimeout(r, ms));
+
+function format_date(date) {
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    if (month < 10) {
+        month = "0" + month
+    }
+    text = year + "-" + month + "-" + day 
+    text += " " + hour + ":" + minute + ":" + second; 
+    return text
+}
+
+async function clock(){
+    while (true) {
+        let now = new Date();
+        let text = "The time is " + format_date(now);
+        console.log(text);
+        await sleep(1000);
+    }
+}
+
+clock()
